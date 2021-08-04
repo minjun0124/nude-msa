5da6430a8d4149c16840ff7098a3c5abfda56271
mjlee <minjun0124@gmail.com>
2021-08-04 19:00:31 +0900

    feat: Eureka server/client(user-service)

---
+++ discovery-service/src/test/java/com/nutritiondesigner/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutritiondesigner.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
