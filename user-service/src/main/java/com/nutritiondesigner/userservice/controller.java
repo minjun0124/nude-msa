46bc1fdc09bf25bd43562549b02dbb73dfb05720
mjlee <minjun0124@gmail.com>
2021-08-06 20:10:42 +0900

    feat: api gateway load balancer

--- user-service/src/main/java/com/nutritiondesigner/userservice/controller.java
+++ user-service/src/main/java/com/nutritiondesigner/userservice/controller.java
@@ -1,13 +1,22 @@
 package com.nutritiondesigner.userservice;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/user-service")
 public class controller {
+
+    private final Environment env;
 
     @GetMapping
     public String test() {
@@ -19,4 +28,10 @@ public class controller {
         return header;
     }
 
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("This is a message from User Sevice on Port %s", env.getProperty("local.server.port"));
+    }
 }
