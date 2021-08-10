91eed19706850608263424044ec1e46990b4017c
mjlee <minjun0124@gmail.com>
2021-08-10 23:14:09 +0900

    feat: kick-off cart service

--- cart-service/cart-service/src/main/java/com/nutrtiondesigner/cartservice/CartServiceApplication.java
+++ cart-service/cart-service/src/main/java/com/nutrtiondesigner/cartservice/CartServiceApplication.java
@@ -1,17 +1,15 @@
-package com.nutritiondesigner.userservice;
+package com.nutrtiondesigner.cartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class UserServiceApplication {
+public class CartServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(CartServiceApplication.class, args);
 	}
 
 	@Bean
